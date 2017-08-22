b3b3dc017e0119d2e3b79693f556ef921a2ef05f
olauby <octave.lauby@beebryte.com>
2017-08-22 15:02:30 +0800

    Comment main

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from qlearning.experience import Experience
 
 
 def play_game(env, agents):
+    """Play a game between the 2 agents, and learn from the game."""
 
     history = {0: {}, 1: {}}
     while not env.game.is_over():
@@ -43,13 +44,17 @@ def play_game(env, agents):
             agents[player_n].update(exp)
 
 
-if __name__ == "__main__":
+def main(iterations):
+    """Play games to teach 2 agents how to play."""
+
+    # Create agents and list of cumulated rewards
     agents = {
         0: Agent(TTT),
         1: Agent(TTT),
     }
     rewards = {0: [], 1: []}
 
+    # Environment for learning
     env = Environment(
         TTT,
         rewards={
@@ -58,22 +63,34 @@ if __name__ == "__main__":
             'lose': -10,
             'invalid': -20,
             'neutral': 0
-    },
+        },
     )
 
-    iterations = 1000
+    # Iterations
     try:
         for i in range(iterations):
             env.reset()
-            print("\r%s / %s" % (i, iterations), end="")
+            print("\r%s / %s" % (i+1, iterations), end="")
             play_game(env, agents)
             for reward_l, agent in zip(rewards.values(), agents.values()):
                 reward_l.append(agent.cumul_reward)
     except KeyboardInterrupt:
         pass
 
+    # Display cumulated rewards
     import matplotlib.pyplot as plt
     plt.plot(rewards[0], label="Creward_%s" % agents[0])
     plt.plot(rewards[1], label="Creward_%s" % agents[1])
     plt.legend()
     plt.show()
+
+
+if __name__ == "__main__":
+    from argparse import ArgumentParser
+    parser = ArgumentParser("Train two agents on TicTacToe.")
+    parser.add_argument(
+        "-i", "--iterations", type=int, required=False, default=1000,
+        help="Number of iterations."
+    )
+    args = parser.parse_args()
+    main(args.iterations)
